c801a87ff89b8540cd2fd2cdde9a7d5c52e2e542
Demidron <andrew.demidyuk@gmail.com>
2021-11-08 01:31:34 +0200

    add models

---
+++ src/app/developer-form/models/front-end-developer.model.ts
@@ -0,0 +1,11 @@
+import { Hobbie } from "./hobbie.model";
+
+export class FrontEndDeveloper {
+    firstName: string="";
+    lastName: string="";
+    dateOfBirth: string="";
+    framework: string="";
+    frameworkVersion: string="";
+    email: string="";
+    hobbies:Hobbie[];
+}
