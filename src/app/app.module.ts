15e0dd1233a01145a3ab94693fa879b690f1eb1d
Demidron <andrew.demidyuk@gmail.com>
2021-11-08 01:29:08 +0200

    Add modules. add bootstrap icons

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,6 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { DeveloperFormModule } from './developer-form/developer-form.module';
 
 @NgModule({
@@ -13,7 +12,6 @@ import { DeveloperFormModule } from './developer-form/developer-form.module';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    NgbModule,
     DeveloperFormModule
   ],
   providers: [],
--- src/app/developer-form/developer-form.module.ts
+++ src/app/developer-form/developer-form.module.ts
@@ -3,17 +3,23 @@ import { CommonModule } from '@angular/common';
 import { DeveloperFormPageComponent } from './pages/developer-form-page/developer-form-page.component';
 import { RouterModule } from '@angular/router';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-
-
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { ValidFieldDirective } from './directives/valid-field.directive';
+import { allIcons, NgxBootstrapIconsModule } from 'ngx-bootstrap-icons';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
-    DeveloperFormPageComponent
+    DeveloperFormPageComponent,
+    ValidFieldDirective
   ],
   imports: [
     CommonModule,
     FormsModule,
     ReactiveFormsModule,
+    NgbModule,
+    NgxBootstrapIconsModule.pick(allIcons),
+    HttpClientModule,
     RouterModule.forChild([
       {path:'',pathMatch:'full',redirectTo:'dev-form'},
       {path:'dev-form',component:DeveloperFormPageComponent},
